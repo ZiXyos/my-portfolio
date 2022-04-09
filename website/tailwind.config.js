73282d8cb2496e5b92ab5d51036738f062dbd288
ZiXyos <killian.moudery@epitech.eu>
2022-04-09 20:48:45 +0200

    [Setup]: 🍃 add tailwindcss

---
+++ website/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkmode: 'class',
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
