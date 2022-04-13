0c282a6f4b3c58ddad9848649decb6ac3311c114
ZiXyos <killian.moudery@epitech.eu>
2022-04-14 00:10:02 +0200

    [Improvement]: improve seo metas

--- website/pages/components/layout/header.tsx
+++ website/pages/components/layout/header.tsx
@@ -4,7 +4,12 @@ const Header = () => (
     <Head>
         <title>Killian.io</title>
         <meta name="description" content="Moudery Killian Website's" />
+        <meta name="author" content="Killian (ZiXyos) Moudery" />
+        <meta name="copyright" content="Copyright ZiXyos" />
+        <meta name="robots" content="follow"/>
         <meta name="google-site-verification" content="gG7-3yOsgdF5CkdC15pDhSACUoT05uCTNs8tY3H0bZo" />
+        <meta name="keywords" content="killian moudery, Killian Moudery, KILLIAN MOUDERY, killian's website, killian, moudery"/>
+        <meta charSet="utf-8"/>
         <link rel="icon" href="/MemojiDev.png" />
     </Head>
 );
