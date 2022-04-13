a3be914672b0514ec78666e988dd453d6ce60a50
ZiXyos <killian.moudery@epitech.eu>
2022-04-14 00:05:41 +0200

    [Improvement]: Add google verification

--- website/pages/components/layout/header.tsx
+++ website/pages/components/layout/header.tsx
@@ -4,6 +4,7 @@ const Header = () => (
     <Head>
         <title>Killian.io</title>
         <meta name="description" content="Moudery Killian Website's" />
+        <meta name="google-site-verification" content="gG7-3yOsgdF5CkdC15pDhSACUoT05uCTNs8tY3H0bZo" />
         <link rel="icon" href="/MemojiDev.png" />
     </Head>
 );
