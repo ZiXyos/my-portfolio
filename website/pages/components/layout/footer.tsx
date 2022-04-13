b20d89c05785d678d25fff8022cad355bcc7eebb
ZiXyos <37801383+ZiXyos@users.noreply.github.com>
2022-04-13 21:33:20 +0200

    [Feat]: add Shitty Version (#1)

--- website/pages/components/layout/footer.tsx
+++ website/pages/components/layout/footer.tsx
@@ -1,8 +1,23 @@
-const Footer = () => (
+import { useEffect, useState } from "react";
 
-    <footer>
-        <h1>Footer</h1>
-    </footer>
-);
+import styles from '/styles/Home.module.css'
+
+const Footer = () => {
+
+    const [bg, setBg] = useState<boolean>(false);
+
+    useEffect( () => {
+
+        setBg(Math.random() < 0.5)
+    }, []);
+
+    return (
+        <footer onClick={() =>  bg ? setBg(false) : setBg(true)}>
+            {
+                bg ? <p>Made 4 U ❤️ so get my ☕️</p> : <p >Made With ❤️ and some ☕️</p>
+            }
+        </footer>
+    )
+};
 
 export default Footer;
--- website/pages/components/layout/layout.tsx
+++ website/pages/components/layout/layout.tsx
@@ -13,7 +13,6 @@ const Layout = ({ children }: LayoutProps) => (
 
     <>
         <Header />
-        <NavBar />
         <main>
             {children}
         </main>
